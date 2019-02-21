fd89ecc47732f03dc5ab7480264383ac9b70001f
Johnatan <ingegonzalez1982@gmail.com>
2019-02-21 12:09:57 -0500

    se cambia ejemplo de js a ejemplo de con objeto en js

--- dist/js/valores.js
+++ dist/js/valores.js
@@ -1,2 +1,4 @@
-let saludo = "palabras"
-console.log(saludo);
+var carro = {marca:"audi" , kilometraje:"5000" ,activo:true   }
+console.log(carro.marca)
+console.log(carro.kilometraje)
+console.log(carro.activo)
